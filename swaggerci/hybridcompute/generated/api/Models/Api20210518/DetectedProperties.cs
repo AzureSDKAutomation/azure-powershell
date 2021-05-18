d9226ca738bea15a3e6ada95986ceb8f61d4297e
SDKAuto <sdkautomation@microsoft.com>
2021-05-18 18:06:23 +0000

    CodeGen from PR 14455 in Azure/azure-rest-api-specs Merge 0babfda2d93f86a7b2f4f0def1035c81ae4d5265 into e0624d2b17a8dcee07acfb08f39508a773a8dc21

---
+++ swaggerci/hybridcompute/generated/api/Models/Api20210518/DetectedProperties.cs
@@ -0,0 +1,30 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.HybridCompute.Models.Api20210518
+{
+    using static Microsoft.Azure.PowerShell.Cmdlets.HybridCompute.Runtime.Extensions;
+
+    /// <summary>Detected properties from the machine.</summary>
+    public partial class DetectedProperties :
+        Microsoft.Azure.PowerShell.Cmdlets.HybridCompute.Models.Api20210518.IDetectedProperties,
+        Microsoft.Azure.PowerShell.Cmdlets.HybridCompute.Models.Api20210518.IDetectedPropertiesInternal
+    {
+
+        /// <summary>Creates an new <see cref="DetectedProperties" /> instance.</summary>
+        public DetectedProperties()
+        {
+
+        }
+    }
+    /// Detected properties from the machine.
+    public partial interface IDetectedProperties :
+        Microsoft.Azure.PowerShell.Cmdlets.HybridCompute.Runtime.IJsonSerializable,
+        Microsoft.Azure.PowerShell.Cmdlets.HybridCompute.Runtime.IAssociativeArray<string>
+    {
+
+    }
+    /// Detected properties from the machine.
+    internal partial interface IDetectedPropertiesInternal
+
+    {
+
+    }
+}
