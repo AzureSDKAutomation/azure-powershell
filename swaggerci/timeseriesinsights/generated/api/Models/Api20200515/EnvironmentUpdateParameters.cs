e35c748199168ad577b847ec94618c7ce5ff70df
SDKAuto <sdkautomation@microsoft.com>
2021-06-08 19:15:38 +0000

    CodeGen from PR 13932 in Azure/azure-rest-api-specs Merge 60587c81f0d80252da760904179103358da8cbcd into 9820e5b189076cb9b329b6968a8d2fc405eef2f6

---
+++ swaggerci/timeseriesinsights/generated/api/Models/Api20200515/EnvironmentUpdateParameters.cs
@@ -0,0 +1,63 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515
+{
+    using static Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Extensions;
+
+    /// <summary>Parameters supplied to the Update Environment operation.</summary>
+    public partial class EnvironmentUpdateParameters :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParameters,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParametersInternal
+    {
+
+        /// <summary>Backing field for <see cref="Kind" /> property.</summary>
+        private Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EnvironmentKind _kind;
+
+        /// <summary>The kind of the environment.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Owned)]
+        public Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EnvironmentKind Kind { get => this._kind; set => this._kind = value; }
+
+        /// <summary>Backing field for <see cref="Tag" /> property.</summary>
+        private Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParametersTags _tag;
+
+        /// <summary>Key-value pairs of additional properties for the environment.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Owned)]
+        public Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParametersTags Tag { get => (this._tag = this._tag ?? new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.EnvironmentUpdateParametersTags()); set => this._tag = value; }
+
+        /// <summary>Creates an new <see cref="EnvironmentUpdateParameters" /> instance.</summary>
+        public EnvironmentUpdateParameters()
+        {
+
+        }
+    }
+    /// Parameters supplied to the Update Environment operation.
+    public partial interface IEnvironmentUpdateParameters :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IJsonSerializable
+    {
+        /// <summary>The kind of the environment.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Info(
+        Required = true,
+        ReadOnly = false,
+        Description = @"The kind of the environment.",
+        SerializedName = @"kind",
+        PossibleTypes = new [] { typeof(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EnvironmentKind) })]
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EnvironmentKind Kind { get; set; }
+        /// <summary>Key-value pairs of additional properties for the environment.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Info(
+        Required = false,
+        ReadOnly = false,
+        Description = @"Key-value pairs of additional properties for the environment.",
+        SerializedName = @"tags",
+        PossibleTypes = new [] { typeof(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParametersTags) })]
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParametersTags Tag { get; set; }
+
+    }
+    /// Parameters supplied to the Update Environment operation.
+    internal partial interface IEnvironmentUpdateParametersInternal
+
+    {
+        /// <summary>The kind of the environment.</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EnvironmentKind Kind { get; set; }
+        /// <summary>Key-value pairs of additional properties for the environment.</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEnvironmentUpdateParametersTags Tag { get; set; }
+
+    }
+}
