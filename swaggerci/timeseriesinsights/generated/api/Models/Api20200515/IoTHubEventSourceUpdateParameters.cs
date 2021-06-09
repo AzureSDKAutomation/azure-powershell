ce204da86b0c49c6a82b016e68b1d659ba512307
SDKAuto <sdkautomation@microsoft.com>
2021-06-09 07:10:46 +0000

    CodeGen from PR 14731 in Azure/azure-rest-api-specs Merge b54cd15691caf369a6619713b1b2740cf785bdb6 into 6f64a862549abbe2a604d82df33b93f0abeb7b64

---
+++ swaggerci/timeseriesinsights/generated/api/Models/Api20200515/IoTHubEventSourceUpdateParameters.cs
@@ -0,0 +1,119 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515
+{
+    using static Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Extensions;
+
+    /// <summary>
+    /// Parameters supplied to the Update Event Source operation to update an IoTHub event source.
+    /// </summary>
+    public partial class IoTHubEventSourceUpdateParameters :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceUpdateParameters,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceUpdateParametersInternal,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IValidates
+    {
+        /// <summary>
+        /// Backing field for Inherited model <see cref= "Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParameters"
+        /// />
+        /// </summary>
+        private Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParameters __eventSourceUpdateParameters = new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.EventSourceUpdateParameters();
+
+        /// <summary>The kind of the event source.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Inherited)]
+        public Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EventSourceKind Kind { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal)__eventSourceUpdateParameters).Kind; }
+
+        /// <summary>Internal Acessors for Kind</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Support.EventSourceKind Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal.Kind { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal)__eventSourceUpdateParameters).Kind; set => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal)__eventSourceUpdateParameters).Kind = value; }
+
+        /// <summary>Internal Acessors for Property</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceMutableProperties Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceUpdateParametersInternal.Property { get => (this._property = this._property ?? new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IoTHubEventSourceMutableProperties()); set { {_property = value;} } }
+
+        /// <summary>Backing field for <see cref="Property" /> property.</summary>
+        private Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceMutableProperties _property;
+
+        /// <summary>Properties of the IoTHub event source.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Owned)]
+        internal Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceMutableProperties Property { get => (this._property = this._property ?? new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IoTHubEventSourceMutableProperties()); set => this._property = value; }
+
+        /// <summary>
+        /// The value of the shared access key that grants the Time Series Insights service read access to the iot hub. This property
+        /// is not shown in event source responses.
+        /// </summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Inlined)]
+        public string SharedAccessKey { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceMutablePropertiesInternal)Property).SharedAccessKey; set => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceMutablePropertiesInternal)Property).SharedAccessKey = value ?? null; }
+
+        /// <summary>Key-value pairs of additional properties for the event source.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Inherited)]
+        public Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersTags Tag { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal)__eventSourceUpdateParameters).Tag; set => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal)__eventSourceUpdateParameters).Tag = value ?? null /* model class */; }
+
+        /// <summary>
+        /// The event property that will be used as the event source's timestamp. If a value isn't specified for timestampPropertyName,
+        /// or if null or empty-string is specified, the event creation time will be used.
+        /// </summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Inlined)]
+        public string TimestampPropertyName { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceMutablePropertiesInternal)Property).TimestampPropertyName; set => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceMutablePropertiesInternal)Property).TimestampPropertyName = value ?? null; }
+
+        /// <summary>Creates an new <see cref="IoTHubEventSourceUpdateParameters" /> instance.</summary>
+        public IoTHubEventSourceUpdateParameters()
+        {
+
+        }
+
+        /// <summary>Validates that this object meets the validation criteria.</summary>
+        /// <param name="eventListener">an <see cref="Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IEventListener" /> instance that will receive validation
+        /// events.</param>
+        /// <returns>
+        /// A < see cref = "global::System.Threading.Tasks.Task" /> that will be complete when validation is completed.
+        /// </returns>
+        public async global::System.Threading.Tasks.Task Validate(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IEventListener eventListener)
+        {
+            await eventListener.AssertNotNull(nameof(__eventSourceUpdateParameters), __eventSourceUpdateParameters);
+            await eventListener.AssertObjectIsValid(nameof(__eventSourceUpdateParameters), __eventSourceUpdateParameters);
+        }
+    }
+    /// Parameters supplied to the Update Event Source operation to update an IoTHub event source.
+    public partial interface IIoTHubEventSourceUpdateParameters :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IJsonSerializable,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParameters
+    {
+        /// <summary>
+        /// The value of the shared access key that grants the Time Series Insights service read access to the iot hub. This property
+        /// is not shown in event source responses.
+        /// </summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Info(
+        Required = false,
+        ReadOnly = false,
+        Description = @"The value of the shared access key that grants the Time Series Insights service read access to the iot hub. This property is not shown in event source responses.",
+        SerializedName = @"sharedAccessKey",
+        PossibleTypes = new [] { typeof(string) })]
+        string SharedAccessKey { get; set; }
+        /// <summary>
+        /// The event property that will be used as the event source's timestamp. If a value isn't specified for timestampPropertyName,
+        /// or if null or empty-string is specified, the event creation time will be used.
+        /// </summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Info(
+        Required = false,
+        ReadOnly = false,
+        Description = @"The event property that will be used as the event source's timestamp. If a value isn't specified for timestampPropertyName, or if null or empty-string is specified, the event creation time will be used.",
+        SerializedName = @"timestampPropertyName",
+        PossibleTypes = new [] { typeof(string) })]
+        string TimestampPropertyName { get; set; }
+
+    }
+    /// Parameters supplied to the Update Event Source operation to update an IoTHub event source.
+    internal partial interface IIoTHubEventSourceUpdateParametersInternal :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceUpdateParametersInternal
+    {
+        /// <summary>Properties of the IoTHub event source.</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IIoTHubEventSourceMutableProperties Property { get; set; }
+        /// <summary>
+        /// The value of the shared access key that grants the Time Series Insights service read access to the iot hub. This property
+        /// is not shown in event source responses.
+        /// </summary>
+        string SharedAccessKey { get; set; }
+        /// <summary>
+        /// The event property that will be used as the event source's timestamp. If a value isn't specified for timestampPropertyName,
+        /// or if null or empty-string is specified, the event creation time will be used.
+        /// </summary>
+        string TimestampPropertyName { get; set; }
+
+    }
+}
---
+++ swaggerci/timeseriesinsights/generated/api/Models/Api20200515/OperationProperties.cs
@@ -0,0 +1,55 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515
+{
+    using static Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Extensions;
+
+    /// <summary>Properties of operation, include metric specifications.</summary>
+    public partial class OperationProperties :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IOperationProperties,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IOperationPropertiesInternal
+    {
+
+        /// <summary>Internal Acessors for ServiceSpecification</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IServiceSpecification Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IOperationPropertiesInternal.ServiceSpecification { get => (this._serviceSpecification = this._serviceSpecification ?? new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.ServiceSpecification()); set { {_serviceSpecification = value;} } }
+
+        /// <summary>Backing field for <see cref="ServiceSpecification" /> property.</summary>
+        private Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IServiceSpecification _serviceSpecification;
+
+        /// <summary>One property of operation, include metric specifications.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Owned)]
+        internal Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IServiceSpecification ServiceSpecification { get => (this._serviceSpecification = this._serviceSpecification ?? new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.ServiceSpecification()); set => this._serviceSpecification = value; }
+
+        /// <summary>Metric specifications of operation.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Inlined)]
+        public Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IMetricSpecification[] ServiceSpecificationMetricSpecification { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IServiceSpecificationInternal)ServiceSpecification).MetricSpecification; set => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IServiceSpecificationInternal)ServiceSpecification).MetricSpecification = value ?? null /* arrayOf */; }
+
+        /// <summary>Creates an new <see cref="OperationProperties" /> instance.</summary>
+        public OperationProperties()
+        {
+
+        }
+    }
+    /// Properties of operation, include metric specifications.
+    public partial interface IOperationProperties :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IJsonSerializable
+    {
+        /// <summary>Metric specifications of operation.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Info(
+        Required = false,
+        ReadOnly = false,
+        Description = @"Metric specifications of operation.",
+        SerializedName = @"metricSpecifications",
+        PossibleTypes = new [] { typeof(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IMetricSpecification) })]
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IMetricSpecification[] ServiceSpecificationMetricSpecification { get; set; }
+
+    }
+    /// Properties of operation, include metric specifications.
+    internal partial interface IOperationPropertiesInternal
+
+    {
+        /// <summary>One property of operation, include metric specifications.</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IServiceSpecification ServiceSpecification { get; set; }
+        /// <summary>Metric specifications of operation.</summary>
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IMetricSpecification[] ServiceSpecificationMetricSpecification { get; set; }
+
+    }
+}
