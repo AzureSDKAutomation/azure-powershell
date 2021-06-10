f4907fda42c0822c2bdf9b07035b8992f30a2a09
SDKAuto <sdkautomation@microsoft.com>
2021-06-10 09:31:43 +0000

    CodeGen from PR 14759 in Azure/azure-rest-api-specs Merge 1f17b34491a4de6464dff79d9f2b0284be55fe50 into ef7ffaa1ae68ab17dba86d8e0be5fd1fb615ef64

---
+++ swaggerci/healthbot/generated/api/Models/Api20201208/HealthBotUpdateParameters.PowerShell.cs
@@ -0,0 +1,147 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208
+{
+    using Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Runtime.PowerShell;
+
+    /// <summary>Parameters for updating a HealthBot.</summary>
+    [System.ComponentModel.TypeConverter(typeof(HealthBotUpdateParametersTypeConverter))]
+    public partial class HealthBotUpdateParameters
+    {
+
+        /// <summary>
+        /// <c>AfterDeserializeDictionary</c> will be called after the deserialization has finished, allowing customization of the
+        /// object before it is returned. Implement this method in a partial class to enable this behavior
+        /// </summary>
+        /// <param name="content">The global::System.Collections.IDictionary content that should be used.</param>
+
+        partial void AfterDeserializeDictionary(global::System.Collections.IDictionary content);
+
+        /// <summary>
+        /// <c>AfterDeserializePSObject</c> will be called after the deserialization has finished, allowing customization of the object
+        /// before it is returned. Implement this method in a partial class to enable this behavior
+        /// </summary>
+        /// <param name="content">The global::System.Management.Automation.PSObject content that should be used.</param>
+
+        partial void AfterDeserializePSObject(global::System.Management.Automation.PSObject content);
+
+        /// <summary>
+        /// <c>BeforeDeserializeDictionary</c> will be called before the deserialization has commenced, allowing complete customization
+        /// of the object before it is deserialized.
+        /// If you wish to disable the default deserialization entirely, return <c>true</c> in the <see "returnNow" /> output parameter.
+        /// Implement this method in a partial class to enable this behavior.
+        /// </summary>
+        /// <param name="content">The global::System.Collections.IDictionary content that should be used.</param>
+        /// <param name="returnNow">Determines if the rest of the serialization should be processed, or if the method should return
+        /// instantly.</param>
+
+        partial void BeforeDeserializeDictionary(global::System.Collections.IDictionary content, ref bool returnNow);
+
+        /// <summary>
+        /// <c>BeforeDeserializePSObject</c> will be called before the deserialization has commenced, allowing complete customization
+        /// of the object before it is deserialized.
+        /// If you wish to disable the default deserialization entirely, return <c>true</c> in the <see "returnNow" /> output parameter.
+        /// Implement this method in a partial class to enable this behavior.
+        /// </summary>
+        /// <param name="content">The global::System.Management.Automation.PSObject content that should be used.</param>
+        /// <param name="returnNow">Determines if the rest of the serialization should be processed, or if the method should return
+        /// instantly.</param>
+
+        partial void BeforeDeserializePSObject(global::System.Management.Automation.PSObject content, ref bool returnNow);
+
+        /// <summary>
+        /// Deserializes a <see cref="global::System.Collections.IDictionary" /> into an instance of <see cref="Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.HealthBotUpdateParameters"
+        /// />.
+        /// </summary>
+        /// <param name="content">The global::System.Collections.IDictionary content that should be used.</param>
+        /// <returns>
+        /// an instance of <see cref="Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParameters"
+        /// />.
+        /// </returns>
+        public static Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParameters DeserializeFromDictionary(global::System.Collections.IDictionary content)
+        {
+            return new HealthBotUpdateParameters(content);
+        }
+
+        /// <summary>
+        /// Deserializes a <see cref="global::System.Management.Automation.PSObject" /> into an instance of <see cref="Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.HealthBotUpdateParameters"
+        /// />.
+        /// </summary>
+        /// <param name="content">The global::System.Management.Automation.PSObject content that should be used.</param>
+        /// <returns>
+        /// an instance of <see cref="Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParameters"
+        /// />.
+        /// </returns>
+        public static Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParameters DeserializeFromPSObject(global::System.Management.Automation.PSObject content)
+        {
+            return new HealthBotUpdateParameters(content);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="HealthBotUpdateParameters" />, deserializing the content from a json string.
+        /// </summary>
+        /// <param name="jsonText">a string containing a JSON serialized instance of this model.</param>
+        /// <returns>an instance of the <see cref="className" /> model class.</returns>
+        public static Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParameters FromJsonString(string jsonText) => FromJson(Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Runtime.Json.JsonNode.Parse(jsonText));
+
+        /// <summary>
+        /// Deserializes a <see cref="global::System.Collections.IDictionary" /> into a new instance of <see cref="Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.HealthBotUpdateParameters"
+        /// />.
+        /// </summary>
+        /// <param name="content">The global::System.Collections.IDictionary content that should be used.</param>
+        internal HealthBotUpdateParameters(global::System.Collections.IDictionary content)
+        {
+            bool returnNow = false;
+            BeforeDeserializeDictionary(content, ref returnNow);
+            if (returnNow)
+            {
+                return;
+            }
+            // actually deserialize
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Sku = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.ISku) content.GetValueForProperty("Sku",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Sku, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.SkuTypeConverter.ConvertFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Identity = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IIdentity) content.GetValueForProperty("Identity",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Identity, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IdentityTypeConverter.ConvertFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Tag = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersTags) content.GetValueForProperty("Tag",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Tag, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.HealthBotUpdateParametersTagsTypeConverter.ConvertFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).SkuName = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.SkuName) content.GetValueForProperty("SkuName",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).SkuName, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.SkuName.CreateFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityPrincipalId = (string) content.GetValueForProperty("IdentityPrincipalId",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityPrincipalId, global::System.Convert.ToString);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityTenantId = (string) content.GetValueForProperty("IdentityTenantId",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityTenantId, global::System.Convert.ToString);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityType = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.ResourceIdentityType?) content.GetValueForProperty("IdentityType",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityType, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.ResourceIdentityType.CreateFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityUserAssignedIdentity = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IIdentityUserAssignedIdentities) content.GetValueForProperty("IdentityUserAssignedIdentity",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityUserAssignedIdentity, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IdentityUserAssignedIdentitiesTypeConverter.ConvertFrom);
+            AfterDeserializeDictionary(content);
+        }
+
+        /// <summary>
+        /// Deserializes a <see cref="global::System.Management.Automation.PSObject" /> into a new instance of <see cref="Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.HealthBotUpdateParameters"
+        /// />.
+        /// </summary>
+        /// <param name="content">The global::System.Management.Automation.PSObject content that should be used.</param>
+        internal HealthBotUpdateParameters(global::System.Management.Automation.PSObject content)
+        {
+            bool returnNow = false;
+            BeforeDeserializePSObject(content, ref returnNow);
+            if (returnNow)
+            {
+                return;
+            }
+            // actually deserialize
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Sku = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.ISku) content.GetValueForProperty("Sku",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Sku, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.SkuTypeConverter.ConvertFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Identity = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IIdentity) content.GetValueForProperty("Identity",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Identity, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IdentityTypeConverter.ConvertFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Tag = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersTags) content.GetValueForProperty("Tag",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).Tag, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.HealthBotUpdateParametersTagsTypeConverter.ConvertFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).SkuName = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.SkuName) content.GetValueForProperty("SkuName",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).SkuName, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.SkuName.CreateFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityPrincipalId = (string) content.GetValueForProperty("IdentityPrincipalId",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityPrincipalId, global::System.Convert.ToString);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityTenantId = (string) content.GetValueForProperty("IdentityTenantId",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityTenantId, global::System.Convert.ToString);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityType = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.ResourceIdentityType?) content.GetValueForProperty("IdentityType",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityType, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Support.ResourceIdentityType.CreateFrom);
+            ((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityUserAssignedIdentity = (Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IIdentityUserAssignedIdentities) content.GetValueForProperty("IdentityUserAssignedIdentity",((Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IHealthBotUpdateParametersInternal)this).IdentityUserAssignedIdentity, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Models.Api20201208.IdentityUserAssignedIdentitiesTypeConverter.ConvertFrom);
+            AfterDeserializePSObject(content);
+        }
+
+        /// <summary>Serializes this instance to a json string.</summary>
+
+        /// <returns>a <see cref="System.String" /> containing this model serialized to JSON text.</returns>
+        public string ToJsonString() => ToJson(null, Microsoft.Azure.PowerShell.Cmdlets.Healthbot.Runtime.SerializationMode.IncludeAll)?.ToString();
+    }
+    /// Parameters for updating a HealthBot.
+    [System.ComponentModel.TypeConverter(typeof(HealthBotUpdateParametersTypeConverter))]
+    public partial interface IHealthBotUpdateParameters
+
+    {
+
+    }
+}
