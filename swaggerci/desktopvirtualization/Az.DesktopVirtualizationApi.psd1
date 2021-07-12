5d0c9c3c5a655255962aeb92a5db285e4adc2d3e
SDKAuto <sdkautomation@microsoft.com>
2021-07-12 03:23:08 +0000

    CodeGen from PR 15149 in Azure/azure-rest-api-specs Merge 6864adf0715ccefeda4a82f74ff098cc7bc77728 into 5e85541d5c4d00459a8e2dcb14512c964e2c1ce9

---
+++ swaggerci/desktopvirtualization/Az.DesktopVirtualizationApi.psd1
@@ -0,0 +1,24 @@
+@{
+  GUID = '4e1d77c2-2dbf-4e88-8568-a622af61921d'
+  RootModule = './Az.DesktopVirtualizationApi.psm1'
+  ModuleVersion = '0.1.0'
+  CompatiblePSEditions = 'Core', 'Desktop'
+  Author = 'Microsoft Corporation'
+  CompanyName = 'Microsoft Corporation'
+  Copyright = 'Microsoft Corporation. All rights reserved.'
+  Description = 'Microsoft Azure PowerShell: DesktopVirtualizationApi cmdlets'
+  PowerShellVersion = '5.1'
+  DotNetFrameworkVersion = '4.7.2'
+  RequiredAssemblies = './bin/Az.DesktopVirtualizationApi.private.dll'
+  FormatsToProcess = './Az.DesktopVirtualizationApi.format.ps1xml'
+  FunctionsToExport = 'Disconnect-AzDesktopVirtualizationApiUserSession', 'Expand-AzDesktopVirtualizationApiMsixImage', 'Get-AzDesktopVirtualizationApiApplication', 'Get-AzDesktopVirtualizationApiApplicationGroup', 'Get-AzDesktopVirtualizationApiDesktop', 'Get-AzDesktopVirtualizationApiHostPool', 'Get-AzDesktopVirtualizationApiHostPoolRegistrationToken', 'Get-AzDesktopVirtualizationApiMsixPackage', 'Get-AzDesktopVirtualizationApiPrivateEndpointConnection', 'Get-AzDesktopVirtualizationApiPrivateLinkResource', 'Get-AzDesktopVirtualizationApiScalingPlan', 'Get-AzDesktopVirtualizationApiSessionHost', 'Get-AzDesktopVirtualizationApiSessionHostConfiguration', 'Get-AzDesktopVirtualizationApiStartMenuItem', 'Get-AzDesktopVirtualizationApiUpdateDetail', 'Get-AzDesktopVirtualizationApiUpdateOperationResult', 'Get-AzDesktopVirtualizationApiUpdateValidationOperationResult', 'Get-AzDesktopVirtualizationApiUserSession', 'Get-AzDesktopVirtualizationApiWorkspace', 'Invoke-AzDesktopVirtualizationApiControlHostPoolUpdate', 'New-AzDesktopVirtualizationApiApplication', 'New-AzDesktopVirtualizationApiApplicationGroup', 'New-AzDesktopVirtualizationApiHostPool', 'New-AzDesktopVirtualizationApiMsixPackage', 'New-AzDesktopVirtualizationApiScalingPlan', 'New-AzDesktopVirtualizationApiSessionHostConfiguration', 'New-AzDesktopVirtualizationApiWorkspace', 'Remove-AzDesktopVirtualizationApiApplication', 'Remove-AzDesktopVirtualizationApiApplicationGroup', 'Remove-AzDesktopVirtualizationApiHostPool', 'Remove-AzDesktopVirtualizationApiMsixPackage', 'Remove-AzDesktopVirtualizationApiPrivateEndpointConnection', 'Remove-AzDesktopVirtualizationApiScalingPlan', 'Remove-AzDesktopVirtualizationApiSessionHost', 'Remove-AzDesktopVirtualizationApiSessionHostConfiguration', 'Remove-AzDesktopVirtualizationApiUserSession', 'Remove-AzDesktopVirtualizationApiWorkspace', 'Send-AzDesktopVirtualizationApiUserSessionMessage', 'Update-AzDesktopVirtualizationApiApplication', 'Update-AzDesktopVirtualizationApiApplicationGroup', 'Update-AzDesktopVirtualizationApiDesktop', 'Update-AzDesktopVirtualizationApiHostPool', 'Update-AzDesktopVirtualizationApiHostPoolPost', 'Update-AzDesktopVirtualizationApiMsixPackage', 'Update-AzDesktopVirtualizationApiScalingPlan', 'Update-AzDesktopVirtualizationApiSessionHost', 'Update-AzDesktopVirtualizationApiSessionHostConfiguration', 'Update-AzDesktopVirtualizationApiWorkspace', '*'
+  AliasesToExport = '*'
+  PrivateData = @{
+    PSData = @{
+      Tags = 'Azure', 'ResourceManager', 'ARM', 'PSModule', 'DesktopVirtualizationApi'
+      LicenseUri = 'https://aka.ms/azps-license'
+      ProjectUri = 'https://github.com/Azure/azure-powershell'
+      ReleaseNotes = ''
+    }
+  }
+}
---
+++ swaggerci/desktopvirtualization/exports/Get-AzDesktopVirtualizationApiPrivateEndpointConnection.ps1
@@ -0,0 +1,200 @@
+
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.Synopsis
+Get a private endpoint connection.
+.Description
+Get a private endpoint connection.
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Inputs
+Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Models.IDesktopVirtualizationApiIdentity
+.Outputs
+Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Models.Api20210513Preview.IPrivateEndpointConnectionWithSystemData
+.Notes
+COMPLEX PARAMETER PROPERTIES
+
+To create the parameters described below, construct a hash table containing the appropriate properties. For information on hash tables, run Get-Help about_Hash_Tables.
+
+INPUTOBJECT <IDesktopVirtualizationApiIdentity>: Identity Parameter
+  [ApplicationGroupName <String>]: The name of the application group
+  [ApplicationName <String>]: The name of the application within the specified application group
+  [DesktopName <String>]: The name of the desktop within the specified desktop group
+  [HostPoolName <String>]: The name of the host pool within the specified resource group
+  [Id <String>]: Resource identity path
+  [MsixPackageFullName <String>]: The version specific package full name of the MSIX package within specified hostpool
+  [PrivateEndpointConnectionName <String>]: The name of the private endpoint connection associated with the Azure resource
+  [ResourceGroupName <String>]: The name of the resource group. The name is case insensitive.
+  [ScalingPlanName <String>]: The name of the scaling plan.
+  [SessionHostName <String>]: The name of the session host within the specified host pool
+  [SubscriptionId <String>]: The ID of the target subscription.
+  [UserSessionId <String>]: The name of the user session within the specified session host
+  [WorkspaceName <String>]: The name of the workspace
+.Link
+https://docs.microsoft.com/en-us/powershell/module/az.desktopvirtualizationapi/get-azdesktopvirtualizationapiprivateendpointconnection
+#>
+function Get-AzDesktopVirtualizationApiPrivateEndpointConnection {
+[OutputType([Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Models.Api20210513Preview.IPrivateEndpointConnectionWithSystemData])]
+[CmdletBinding(DefaultParameterSetName='List', PositionalBinding=$false)]
+param(
+    [Parameter(ParameterSetName='Get', Mandatory)]
+    [Parameter(ParameterSetName='List', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Path')]
+    [System.String]
+    # The name of the host pool within the specified resource group
+    ${HostPoolName},
+
+    [Parameter(ParameterSetName='Get', Mandatory)]
+    [Parameter(ParameterSetName='Get1', Mandatory)]
+    [Alias('PrivateEndpointConnectionName')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Path')]
+    [System.String]
+    # The name of the private endpoint connection associated with the Azure resource
+    ${Name},
+
+    [Parameter(ParameterSetName='Get', Mandatory)]
+    [Parameter(ParameterSetName='Get1', Mandatory)]
+    [Parameter(ParameterSetName='List', Mandatory)]
+    [Parameter(ParameterSetName='List1', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Path')]
+    [System.String]
+    # The name of the resource group.
+    # The name is case insensitive.
+    ${ResourceGroupName},
+
+    [Parameter(ParameterSetName='Get')]
+    [Parameter(ParameterSetName='Get1')]
+    [Parameter(ParameterSetName='List')]
+    [Parameter(ParameterSetName='List1')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Path')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Runtime.DefaultInfo(Script='(Get-AzContext).Subscription.Id')]
+    [System.String[]]
+    # The ID of the target subscription.
+    ${SubscriptionId},
+
+    [Parameter(ParameterSetName='Get1', Mandatory)]
+    [Parameter(ParameterSetName='List1', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Path')]
+    [System.String]
+    # The name of the workspace
+    ${WorkspaceName},
+
+    [Parameter(ParameterSetName='GetViaIdentity', Mandatory, ValueFromPipeline)]
+    [Parameter(ParameterSetName='GetViaIdentity1', Mandatory, ValueFromPipeline)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Path')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Models.IDesktopVirtualizationApiIdentity]
+    # Identity Parameter
+    # To construct, see NOTES section for INPUTOBJECT properties and create a hash table.
+    ${InputObject},
+
+    [Parameter()]
+    [Alias('AzureRMContext', 'AzureCredential')]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Azure')]
+    [System.Management.Automation.PSObject]
+    # The credentials, account, tenant, and subscription used for communication with Azure.
+    ${DefaultProfile},
+
+    [Parameter(DontShow)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Wait for .NET debugger to attach
+    ${Break},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Runtime')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Runtime.SendAsyncStep[]]
+    # SendAsync Pipeline Steps to be appended to the front of the pipeline
+    ${HttpPipelineAppend},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Runtime')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Runtime.SendAsyncStep[]]
+    # SendAsync Pipeline Steps to be prepended to the front of the pipeline
+    ${HttpPipelinePrepend},
+
+    [Parameter(DontShow)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Runtime')]
+    [System.Uri]
+    # The URI for the proxy server to use
+    ${Proxy},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Runtime')]
+    [System.Management.Automation.PSCredential]
+    # Credentials for a proxy server to use for the remote call
+    ${ProxyCredential},
+
+    [Parameter(DontShow)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DesktopVirtualizationApi.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Use the default credentials for the proxy
+    ${ProxyUseDefaultCredentials}
+)
+
+begin {
+    try {
+        $outBuffer = $null
+        if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer)) {
+            $PSBoundParameters['OutBuffer'] = 1
+        }
+        $parameterSet = $PSCmdlet.ParameterSetName
+        $mapping = @{
+            Get = 'Az.DesktopVirtualizationApi.private\Get-AzDesktopVirtualizationApiPrivateEndpointConnection_Get';
+            Get1 = 'Az.DesktopVirtualizationApi.private\Get-AzDesktopVirtualizationApiPrivateEndpointConnection_Get1';
+            GetViaIdentity = 'Az.DesktopVirtualizationApi.private\Get-AzDesktopVirtualizationApiPrivateEndpointConnection_GetViaIdentity';
+            GetViaIdentity1 = 'Az.DesktopVirtualizationApi.private\Get-AzDesktopVirtualizationApiPrivateEndpointConnection_GetViaIdentity1';
+            List = 'Az.DesktopVirtualizationApi.private\Get-AzDesktopVirtualizationApiPrivateEndpointConnection_List';
+            List1 = 'Az.DesktopVirtualizationApi.private\Get-AzDesktopVirtualizationApiPrivateEndpointConnection_List1';
+        }
+        if (('Get', 'Get1', 'List', 'List1') -contains $parameterSet -and -not $PSBoundParameters.ContainsKey('SubscriptionId')) {
+            $PSBoundParameters['SubscriptionId'] = (Get-AzContext).Subscription.Id
+        }
+        $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand(($mapping[$parameterSet]), [System.Management.Automation.CommandTypes]::Cmdlet)
+        $scriptCmd = {& $wrappedCmd @PSBoundParameters}
+        $steppablePipeline = $scriptCmd.GetSteppablePipeline($MyInvocation.CommandOrigin)
+        $steppablePipeline.Begin($PSCmdlet)
+    } catch {
+        throw
+    }
+}
+
+process {
+    try {
+        $steppablePipeline.Process($_)
+    } catch {
+        throw
+    }
+}
+
+end {
+    try {
+        $steppablePipeline.End()
+    } catch {
+        throw
+    }
+}
+}
